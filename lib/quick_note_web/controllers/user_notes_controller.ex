a2c67780828989833cd5d24ea9e7e57def1f5c1b
Takuya Hirata <takuyahirata4@gmail.com>
2022-09-16 23:15:56 -0400

    Feat: note show page

--- lib/quick_note_web/controllers/user_notes_controller.ex
+++ lib/quick_note_web/controllers/user_notes_controller.ex
@@ -21,4 +21,9 @@ defmodule QuickNoteWeb.UserNotesController do
         redirect(conn, to: Routes.user_folders_path(conn, :show, folder_id))
     end
   end
+
+  def show(conn, %{"note_id" => note_id, "id" => folder_id}) do
+    note = Notes.get_note_by_id(conn.assigns.current_user.id, note_id)
+    render(conn, "show.html", note: note, folder_id: folder_id)
+  end
 end
