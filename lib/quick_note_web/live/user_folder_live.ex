9527b6a4693f4e396de7f8a94c0ed6a8364f168e
Takuya Hirata <takuyahirata4@gmail.com>
2022-10-04 08:28:36 -0400

    Feat: create folders

--- lib/quick_note_web/live/user_folder_live.ex
+++ lib/quick_note_web/live/user_folder_live.ex
@@ -6,13 +6,10 @@ defmodule QuickNoteWeb.UserFolderLive do
   alias QuickNoteWeb.LayoutComponent
 
   def mount(_params, session, socket) do
-    folders = get_folders(session, socket)
-    {:ok, assign(socket, folders: folders, user_id: session["user_id"])}
+    {:ok, assign(socket, user_id: session["user_id"])}
   end
 
   def handle_params(_params, _url, socket) do
-    IO.puts("\n\n\nRUN\n\n\n")
-
     case socket.assigns.live_action do
       :new ->
         LayoutComponent.show_modal(QuickNoteWeb.FolderFormComponent, %{
@@ -24,14 +21,8 @@ defmodule QuickNoteWeb.UserFolderLive do
         LayoutComponent.hide_modal()
     end
 
-    {:noreply, socket}
-  end
+    folders = Notes.get_folders_with_note_counts_by_user_id(socket.assigns.user_id)
 
-  defp get_folders(session, socket) do
-    if connected?(socket) do
-      Notes.get_folders_with_note_counts_by_user_id(session["user_id"])
-    else
-      []
-    end
+    {:noreply, assign(socket, folders: folders)}
   end
 end
