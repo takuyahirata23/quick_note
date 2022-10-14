76e228a63f3130d7feee04703cbd4c2f9f1d8f87
Takuya Hirata <takuyahirata4@gmail.com>
2022-10-13 21:39:56 -0400

    check origin

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :quick_note, QuickNoteWeb.Endpoint,
-  url: [host: "quick-note.gigalixirapp.com", port: 443, scheme: "https"],
+  url: [host: "quick-note.gigalixirapp.com", port: 443],
+  check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
