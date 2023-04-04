7d61fae236c6af8dbf56b92c06edb01b842d301f
Takuya Hirata <takuyahirata4@gmail.com>
2023-04-04 07:56:10 -0400

    Chore: clean up unused routes and deal with warnings

--- lib/quick_note_web/live/note_edit_form_component.ex
+++ lib/quick_note_web/live/note_edit_form_component.ex
@@ -28,41 +28,44 @@ defmodule QuickNoteWeb.NoteEditFormComponent do
   def render(assigns) do
     ~H"""
     <div>
-    <.form let={f} for={@changeset} action="#" phx-submit="edit" phx-target={@myself}>
-    <%= if @changeset.action do %>
-      <div class="alert alert-danger">
-      <p>Oops, something went wrong! Please check the errors below.</p>
-      </div>
-      <% end %>
+      <.form :let={f} for={@changeset} action="#" phx-submit="edit" phx-target={@myself}>
+        <%= if @changeset.action do %>
+          <div class="alert alert-danger">
+            <p>Oops, something went wrong! Please check the errors below.</p>
+          </div>
+        <% end %>
 
-    <div class="flex flex-col">
-    <div class="flex flex-col gap-y-1">
-    <%= label f, :title, class: "text-md" %>
-    <%= text_input f, :title, required: true, class: "border-none bg-light rounded-md"  %>
-    <%= error_tag f, :title %>
-    </div>
-    <div class="flex flex-col gap-y-1">
-    <%= label f, :copy, class: "text-md" %>
-    <%= text_input f, :copy, required: true, class: "border-none bg-light rounded-md"  %>
-    <%= error_tag f, :copy %>
-    </div>
-    <div class="flex flex-col gap-y-1">
-    <%= label f, :description, class: "text-md" %>
-    <%= textarea f, :description, class: "border-none bg-light rounded-md", rows: 4  %>
-    <%= error_tag f, :description %>
-    </div>
-        <div class="flex gap-x-2 items-center mt-2">
-        <%= label f, :is_pinned, "Pin" ,class: "text-sm" %>
-        <%= checkbox f, :is_pinned %>
+        <div class="flex flex-col">
+          <div class="flex flex-col gap-y-1">
+            <%= label(f, :title, class: "text-md") %>
+            <%= text_input(f, :title, required: true, class: "border-none bg-light rounded-md") %>
+            <%= error_tag(f, :title) %>
+          </div>
+          <div class="flex flex-col gap-y-1">
+            <%= label(f, :copy, class: "text-md") %>
+            <%= text_input(f, :copy, required: true, class: "border-none bg-light rounded-md") %>
+            <%= error_tag(f, :copy) %>
+          </div>
+          <div class="flex flex-col gap-y-1">
+            <%= label(f, :description, class: "text-md") %>
+            <%= textarea(f, :description, class: "border-none bg-light rounded-md", rows: 4) %>
+            <%= error_tag(f, :description) %>
+          </div>
+          <div class="flex gap-x-2 items-center mt-2">
+            <%= label(f, :is_pinned, "Pin", class: "text-sm") %>
+            <%= checkbox(f, :is_pinned) %>
+          </div>
+          <div class="flex gap-x-4 mt-8">
+            <.link
+              patch={Routes.user_note_path(@socket, :index, @folder_id, @note.id)}
+              class="bg-accent rounded-md p-2 text-center flex-1"
+            >
+              Cancel
+            </.link>
+            <%= submit("Update", class: "w-1/2 mx-auto bg-primary text-white py-2 rounded-md flex-1") %>
+          </div>
         </div>
-    <div class="flex gap-x-4 mt-8">
-    <.link patch={Routes.user_note_path(@socket, :index, @folder_id, @note.id)} class="bg-accent rounded-md p-2 text-center flex-1">
-    Cancel
-    </.link>
-    <%= submit "Update", class: "w-1/2 mx-auto bg-primary text-white py-2 rounded-md flex-1" %>
-    </div>
-    </div>
-    </.form>
+      </.form>
     </div>
     """
   end
